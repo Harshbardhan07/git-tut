9cacadac6ccf691b1ba3b762e509cf6aa0cbc90a
harsh bardhan <hbardhan0799@gmail.com>
2024-05-11 18:27:33 +0530

    v1

---
+++ start.py
@@ -0,0 +1,7 @@
+# this is basic program for python
+'''hello i am using this to use multiline comment to use it for many lines'''
+print("hello world\ngogo")
+#\n for new line
+print(7)
+#In python we can use two output at same time by using comma 
+print("Hello",7)
